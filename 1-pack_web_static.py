0a878b6cef3f267f45401aa1562a49f0ef422d28
Mahi <mahlet7000@gmail.com>
2024-07-10 09:37:12 -0700

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,13 +5,13 @@ from fabric.api import *
 from datetime import datetime
 
 
-def do_pack(c):
+def do_pack():
     """make archive"""
     t = datetime.now()
     archive = 'web_static_{}.tgz'.format(t.strftime("%Y%m%d%H%M%S"))
     local('mkdir -p versions')
     c = local('tar -cvzf versions/{} web_static'.format(archive))
-    if create is not None:
+    if c is not None:
         return archive
     else:
         return None
