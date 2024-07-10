715e8636c1a9c32f129e47639736f1d80fa0a050
Mahi <mahlet7000@gmail.com>
2024-07-10 05:22:53 -0700

    sql

--- models/city.py
+++ models/city.py
@@ -2,7 +2,7 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel
 
-class City(BaseModel, Base):
+class City(BaseModel):
     """ The city class, contains state ID and name """
 
     name = ""
