792a37aeeb112844037e16b75a94f404fd6c7de1
Mahi <mahlet7000@gmail.com>
2024-07-28 03:25:16 -0700

    all doesn't work

--- models/base_model.py
+++ models/base_model.py
@@ -12,16 +12,17 @@ Base = declarative_base()
 
 class BaseModel:
     """A base class for all hbnb models"""
-    id = Column(String(60), nullable=False, primary_key=True)
-    created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
-    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow)
+    if models.env == 'db':
+        id = Column(String(60), nullable=False, primary_key=True)
+        created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
+        updated_at = Column(DateTime, nullable=False, default=datetime.utcnow)
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
         else:
             for key, value in kwargs.items():
                 if key != "__class__":
@@ -40,7 +41,7 @@ class BaseModel:
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.utcnow()
         models.storage.new(self)
         models.storage.save()
 
@@ -52,7 +53,8 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
-        dictionary.pop('_sa_instance_state', None)
+        if "_sa_instance_state" in dictionary:
+            del dictionary["_sa_instance_state"]
         return dictionary
 
     def delete(self):
--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,43 +1,44 @@
 #!/usr/bin/python3
-""" Database engine new """
+"""DBStorage with mysql"""
+
 import os
 import models
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
+import sqlalchemy
 from models.base_model import BaseModel, Base
 from models.amenity import Amenity
 from models.city import City
+from models.state import State
 from models.place import Place
 from models.review import Review
-from models.state import State
 from models.user import User
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
 
-classes = {"City": City, "State": State, "User": User,
-           "Amenity": Amenity, "Place": Place, "Review": Review}
+classes = {"Amenity": Amenity, "City": City, "State": State,
+           "Place": Place, "Review": Review, "User": User}
 
 
 class DBStorage:
-    """New engine"""
+    """MySQL Database"""
     __engine = None
     __session = None
 
     def __init__(self):
-        """initialization create engine"""
+        """Initialize class DBStorage"""
         user = os.getenv('HBNB_MYSQL_USER')
-        password = os.getenv('HBNB_MYSQL_PWD')
-        host = os.getenv('HBNB_MYSQL_HOST', 'localhost')
+        pwd = os.getenv('HBNB_MYSQL_PWD')
+        host = os.getenv('HBNB_MYSQL_HOST')
         db = os.getenv('HBNB_MYSQL_DB')
         env = os.getenv('HBNB_ENV')
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
+                                      format(user, pwd, host, db),
+                                      pool_pre_ping=True)
 
-        self.__engine = create_engine(
-                f"mysql+mysqldb://{user}:{password}@{host}/{db}",
-                pool_pre_ping=True)
-
-        if env == 'test':
+        if env == "test":
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """Query on current database session"""
+        """current database"""
         obj_dict = {}
         for att in classes:
             if cls is None or cls is classes[att] or cls is att:
@@ -48,25 +49,25 @@ class DBStorage:
         return obj_dict
 
     def new(self, obj):
-        """Add object to the current database session"""
+        """add to the database"""
         self.__session.add(obj)
 
     def save(self):
-        """Commit all changes of the current database session"""
+        """commit to session"""
         self.__session.commit()
 
     def delete(self, obj=None):
-        """Delete from the current database session obj if not none"""
+        """delete from current database"""
         if obj is not None:
             self.__session.delete(obj)
 
     def reload(self):
-        """Create all tables in the database"""
+        """reload from database"""
         Base.metadata.create_all(self.__engine)
         session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(session_factory)
         self.__session = Session
 
     def close(self):
-        """Close the engine"""
+        """remove session"""
         self.__session.remove()
